a5e727bddf1d07fa0c919ae7882849f0df554c75
Gavin Reid <gavinjohnreid@gmail.com>
2025-02-23 11:09:21 +0100

    Fixed Gunicorn startup issue

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn "app.main:app"
+web: gunicorn -c gunicorn_config.py run:app
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,3 +4,5 @@ limit_request_field_size = 0
 limit_request_line = 0
 timeout = 300  # Allow longer upload times
 worker_connections = 1000  # Allow more simultaneous connections
+workers = 3  # Adjust based on Azure App Service plan
+bind = "0.0.0.0:8000"
